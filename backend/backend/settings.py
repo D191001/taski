cfd0589cc1f040f64177aba1d3871c9c073a88a8
Dmitry <103212557+D191001@users.noreply.github.com>
2023-07-10 16:07:45 -0500

    Update settings.py
    Allow hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,6 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['158.160.66.226', '127.0.0.1', 'localhost']
 
 
